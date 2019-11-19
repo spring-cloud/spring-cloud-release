c68487e512d670bd32665863128a755699fefc72
buildmaster <buildmaster@springframework.org>
2019-11-19 14:10:27 +0000

    Sync docs from master to gh-pages

---
+++ docs/target/build-docs/org/springframework/cloud/internal/Main.java
@@ -0,0 +1,54 @@
+package org.springframework.cloud.internal;
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class Main
+  extends java.lang.Object  implements
+    groovy.lang.GroovyObject {
+;
+@groovy.transform.Generated() @groovy.transform.Internal() public  groovy.lang.MetaClass getMetaClass() { return (groovy.lang.MetaClass)null;}
+@groovy.transform.Generated() @groovy.transform.Internal() public  void setMetaClass(groovy.lang.MetaClass mc) { }
+@groovy.transform.Generated() @groovy.transform.Internal() public  java.lang.Object invokeMethod(java.lang.String method, java.lang.Object arguments) { return null;}
+@groovy.transform.Generated() @groovy.transform.Internal() public  java.lang.Object getProperty(java.lang.String property) { return null;}
+@groovy.transform.Generated() @groovy.transform.Internal() public  void setProperty(java.lang.String property, java.lang.Object value) { }
+@groovy.transform.CompileStatic() public static  void main(java.lang.String... args) { }
+public static class Generator
+  extends java.lang.Object  implements
+    groovy.lang.GroovyObject {
+;
+@groovy.transform.Generated() @groovy.transform.Internal() public  groovy.lang.MetaClass getMetaClass() { return (groovy.lang.MetaClass)null;}
+@groovy.transform.Generated() @groovy.transform.Internal() public  void setMetaClass(groovy.lang.MetaClass mc) { }
+@groovy.transform.Generated() @groovy.transform.Internal() public  java.lang.Object invokeMethod(java.lang.String method, java.lang.Object arguments) { return null;}
+@groovy.transform.Generated() @groovy.transform.Internal() public  java.lang.Object getProperty(java.lang.String property) { return null;}
+@groovy.transform.Generated() @groovy.transform.Internal() public  void setProperty(java.lang.String property, java.lang.Object value) { }
+public  void generate(java.lang.String outputFile, java.lang.String inclusionPattern) { }
+protected  boolean resourceNameContainsPattern(org.springframework.core.io.Resource resource) { return false;}
+protected  org.springframework.core.io.Resource[] getResources() { return (org.springframework.core.io.Resource[])null;}
+}
+@groovy.transform.CompileStatic() public static class ConfigValue
+  extends java.lang.Object  implements
+    groovy.lang.GroovyObject {
+;
+public ConfigValue
+() {}
+public ConfigValue
+(java.lang.String name, java.lang.String description, java.lang.Object defaultValue) {}
+@groovy.transform.Generated() @groovy.transform.Internal() public  groovy.lang.MetaClass getMetaClass() { return (groovy.lang.MetaClass)null;}
+@groovy.transform.Generated() @groovy.transform.Internal() public  void setMetaClass(groovy.lang.MetaClass mc) { }
+@groovy.transform.Generated() @groovy.transform.Internal() public  java.lang.Object invokeMethod(java.lang.String method, java.lang.Object arguments) { return null;}
+@groovy.transform.Generated() @groovy.transform.Internal() public  java.lang.Object getProperty(java.lang.String property) { return null;}
+@groovy.transform.Generated() @groovy.transform.Internal() public  void setProperty(java.lang.String property, java.lang.Object value) { }
+public  java.lang.String getName() { return (java.lang.String)null;}
+public  void setName(java.lang.String value) { }
+public  java.lang.String getDescription() { return (java.lang.String)null;}
+public  void setDescription(java.lang.String value) { }
+public  java.lang.Object getDefaultValue() { return null;}
+public  void setDefaultValue(java.lang.Object value) { }
+public  java.lang.String toString() { return (java.lang.String)null;}
+}
+}
